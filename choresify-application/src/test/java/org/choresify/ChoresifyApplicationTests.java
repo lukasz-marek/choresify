83013eeec4d1ad1398253090947d20d58172273a
Łukasz Marek <marek.lucas@gmail.com>
2023-01-30 20:38:29 +0100

    initial project setup

---
+++ choresify-application/src/test/java/org/choresify/ChoresifyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.choresify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoresifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
