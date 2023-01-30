72091da8bfeaf524998c5b635d0afeedcefc03ba
Łukasz Marek <marek.lucas@gmail.com>
2023-01-30 21:19:22 +0100

    move common configuration to top-level project

--- choresify-application/src/test/java/org/choresify/ChoresifyApplicationTests.java
+++ choresify-application/src/test/java/org/choresify/ChoresifyApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ChoresifyApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
