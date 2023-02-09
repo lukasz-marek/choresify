75b674961a2c1970029155fbd3239d0eb62450ef
Łukasz Marek <marek.lucas@gmail.com>
2023-02-09 21:24:09 +0100

    limit usage of @Data for JPA entities

--- choresify-application/src/main/java/org/choresify/application/member/adapter/driven/postgres/entity/MemberEntity.java
+++ choresify-application/src/main/java/org/choresify/application/member/adapter/driven/postgres/entity/MemberEntity.java
@@ -7,21 +7,26 @@ import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import lombok.experimental.FieldDefaults;
 
+@Builder
+@Getter
+@Setter
+@ToString(onlyExplicitlyIncluded = true)
+@FieldDefaults(level = AccessLevel.PRIVATE)
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
-@Builder
-@FieldDefaults(level = AccessLevel.PRIVATE)
 @Entity(name = "member")
 public class MemberEntity {
+  @ToString.Include
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   Long id;
 
-  String nickname;
-  String emailAddress;
+  @ToString.Include String nickname;
+  @ToString.Include String emailAddress;
 }
