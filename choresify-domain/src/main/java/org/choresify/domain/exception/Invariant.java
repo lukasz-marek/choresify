0a1fb2365792d055d4fea20f4b8fed0321d60da7
Łukasz Marek <marek.lucas@gmail.com>
2023-02-11 23:03:50 +0100

    add logging

--- choresify-domain/src/main/java/org/choresify/domain/exception/Invariant.java
+++ choresify-domain/src/main/java/org/choresify/domain/exception/Invariant.java
@@ -1,10 +1,14 @@
 package org.choresify.domain.exception;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public final class Invariant {
   private Invariant() {}
 
   public static void assertTrue(boolean invariant, String message) {
     if (!invariant) {
+      log.debug("Invariant violation detected: {}", message);
       throw new InvariantViolationException(message);
     }
   }
