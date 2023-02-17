b41145b2e16774036ff0ff418361d6f655afaefb
Łukasz Marek <marek.lucas@gmail.com>
2023-02-17 22:50:10 +0100

    integrate normalization to NewMember

--- choresify-domain/src/main/java/org/choresify/domain/member/model/NewMember.java
+++ choresify-domain/src/main/java/org/choresify/domain/member/model/NewMember.java
@@ -1,6 +1,8 @@
 package org.choresify.domain.member.model;
 
 import lombok.Builder;
+import org.choresify.domain.common.sanitation.EmailAddressSanitizer;
+import org.choresify.domain.common.sanitation.NameSanitizer;
 import org.choresify.domain.exception.Invariants;
 
 @Builder
@@ -8,5 +10,7 @@ public record NewMember(String nickname, String emailAddress) {
   public NewMember {
     Invariants.requireNonNull(nickname, "nickname");
     Invariants.requireNonNull(emailAddress, "emailAddress");
+    emailAddress = EmailAddressSanitizer.sanitize(emailAddress);
+    nickname = NameSanitizer.sanitize(nickname);
   }
 }
--- choresify-domain/src/test/java/org/choresify/domain/member/model/NewMemberTest.java
+++ choresify-domain/src/test/java/org/choresify/domain/member/model/NewMemberTest.java
@@ -1,5 +1,7 @@
 package org.choresify.domain.member.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.assertj.core.api.Assertions;
 import org.choresify.domain.exception.InvariantViolationException;
 import org.junit.jupiter.api.Test;
@@ -15,7 +17,7 @@ class NewMemberTest {
             InvariantViolationException.class);
 
     // then
-    Assertions.assertThat(throwable).hasMessageContaining("nickname must not be null");
+    assertThat(throwable).hasMessageContaining("nickname must not be null");
   }
 
   @Test
@@ -27,6 +29,46 @@ class NewMemberTest {
             InvariantViolationException.class);
 
     // then
-    Assertions.assertThat(throwable).hasMessageContaining("emailAddress must not be null");
+    assertThat(throwable).hasMessageContaining("emailAddress must not be null");
+  }
+
+  @Test
+  void canCreateMemberWithAllRequiredFields() {
+    // when
+    var newMember =
+        NewMember.builder().nickname("a nickname").emailAddress("email@example.com").build();
+
+    // then
+    assertThat(newMember.nickname()).isEqualTo("a nickname");
+    assertThat(newMember.emailAddress()).isEqualTo("email@example.com");
+  }
+
+  @Test
+  void emailAddressIsNormalized() {
+    // given
+    var newMember =
+        NewMember.builder().nickname("a nickname").emailAddress(" EmAil@ exa mple.coM  ").build();
+
+    // when
+    var email = newMember.emailAddress();
+
+    // then
+    assertThat(email).isEqualTo("email@example.com");
+  }
+
+  @Test
+  void nicknameIsNormalized() {
+    // given
+    var newMember =
+        NewMember.builder()
+            .nickname("  a   nickname  \n\t")
+            .emailAddress("email@example.com")
+            .build();
+
+    // when
+    var nickname = newMember.nickname();
+
+    // then
+    assertThat(nickname).isEqualTo("a nickname");
   }
 }
