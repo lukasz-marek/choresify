44cbbd5c406a2a07a55cb93c462ad82c6a0e5a9e
Łukasz Marek <marek.lucas@gmail.com>
2023-02-17 22:57:43 +0100

    document using findbugs

---
+++ choresify-domain/src/main/java/org/choresify/domain/common/sanitation/package-info.java
@@ -0,0 +1,10 @@
+@DefaultAnnotationForParameters(Nullable.class)
+@DefaultAnnotationForFields(NonNull.class)
+@DefaultAnnotationForMethods(Nullable.class)
+package org.choresify.domain.common.sanitation;
+
+import edu.umd.cs.findbugs.annotations.DefaultAnnotationForFields;
+import edu.umd.cs.findbugs.annotations.DefaultAnnotationForMethods;
+import edu.umd.cs.findbugs.annotations.DefaultAnnotationForParameters;
+import edu.umd.cs.findbugs.annotations.NonNull;
+import edu.umd.cs.findbugs.annotations.Nullable;
