bf9d0a789b6e054b5d07eb35e9d325f6fa6ffd01
Łukasz Marek <marek.lucas@gmail.com>
2023-02-18 19:48:41 +0100

    fake implemented for Households class

---
+++ choresify-domain/src/test/java/org/choresify/fixtures/household/port/InMemoryHouseholds.java
@@ -0,0 +1,25 @@
+package org.choresify.fixtures.household.port;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import org.choresify.domain.household.model.Household;
+import org.choresify.domain.household.model.NewHousehold;
+import org.choresify.domain.household.port.Households;
+
+public final class InMemoryHouseholds implements Households {
+  private final Map<Long, Household> storage = new LinkedHashMap<>();
+  private long nextId = 0;
+
+  @Override
+  public Household insert(NewHousehold newHousehold) {
+    var household =
+        Household.builder()
+            .id(nextId++)
+            .name(newHousehold.name())
+            .members(newHousehold.members())
+            .version(0)
+            .build();
+    storage.put(household.id(), household);
+    return household;
+  }
+}
